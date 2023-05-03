ffea7f72a968dee4cb5267904f87b4c49b730a6c
AcompliceCC <111264378+AcompliceCC@users.noreply.github.com>
2023-05-03 09:33:24 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -11,17 +11,17 @@ except ImportError as e:
 def ridge(data):
     X,y=read_data()
     X=(X-np.min(X))/(np.max(X)-np.min(X))
-    # z=np.matmul(X.T,X)+np.eye(X.shape[1])*0.2
-    # weight=np.matmul(np.linalg.inv(z),np.matmul(X.T,y))
-    m, n = X.shape
-    weight = np.zeros(n)
-    max_iterations = 1000000
-    for i in range(max_iterations):
+    z=np.matmul(X.T,X)+np.eye(X.shape[1])*0.01
+    weight=np.matmul(np.linalg.inv(z),np.matmul(X.T,y))
+   # m, n = X.shape
+   # weight = np.zeros(n)
+  #  max_iterations = 1000000
+    #for i in range(max_iterations):
         # 计算梯度
-        grad = (np.matmul(X.T, (np.matmul(X, weight) - y))) + 1e-12 * np.sign(weight)
-        weight = weight - 1e-12 * grad
-        if np.linalg.norm(grad) < 0.0001:
-            break
+      #  grad = (np.matmul(X.T, (np.matmul(X, weight) - y))) + 1e-12 * np.sign(weight)
+       # weight = weight - 1e-12 * grad
+        #if np.linalg.norm(grad) < 0.0001:
+          #  break
     return weight @ data
     
 def lasso(data):
